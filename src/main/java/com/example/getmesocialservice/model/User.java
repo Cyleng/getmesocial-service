8bcb5b7675e7763eec6485feb3c024b4f3c6ff20
Chuanyu Leng <lengchuanyu@outlook.com>
2023-01-20 01:34:47 -0600

    initialize project

---
+++ src/main/java/com/example/getmesocialservice/model/User.java
@@ -0,0 +1,10 @@
+package com.example.getmesocialservice.model;
+
+public class User {
+
+    private String name;
+    private String address;
+    private int age;
+    private String profilePicUrl;
+
+}
---
+++ src/main/java/com/example/getmesocialservice/resource/UserResource.java
@@ -0,0 +1,4 @@
+package com.example.getmesocialservice.resource;
+
+public class UserResource {
+}
