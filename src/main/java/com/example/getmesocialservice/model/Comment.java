fe8eacaaa2399070573f7da7833e36d55c74e625
Chuanyu Leng <lengchuanyu@outlook.com>
2023-02-11 23:51:24 -0600

    update comment model, add missing properties

--- src/main/java/com/example/getmesocialservice/model/Comment.java
+++ src/main/java/com/example/getmesocialservice/model/Comment.java
@@ -2,13 +2,21 @@ package com.example.getmesocialservice.model;
 
 import nonapi.io.github.classgraph.json.Id;
 
+import java.util.Date;
+
 public class Comment {
     @Id
     private String id;
     private String photoId;
 
-    public Comment(String photoId) {
+    private String message;
+    private String createdBy;
+    private Date dateCreated;
+
+    public Comment(String photoId, String message, String createdBy) {
         this.photoId = photoId;
+        this.message = message;
+        this.createdBy = createdBy;
     }
 
     public String getPhotoId() {
@@ -17,5 +25,21 @@ public class Comment {
 
     public void setPhotoId(String photoId) {
         this.photoId = photoId;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
     }
 }
